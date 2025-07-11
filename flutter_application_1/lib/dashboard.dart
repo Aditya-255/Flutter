8621771581dd123efc66c15d2e33cf530354633f
Aditya Dodiya <aadityadodiya01@gmail.com>
2025-07-11 09:39:31 +0530

    add file

---
+++ flutter_application_1/lib/dashboard.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatelessWidget {
+  const Dashboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Dashboard")),
+      body: Card(
+        child: Column(
+          children: [
+            Container(height: 100, color: Colors.red),
+            Text(
+              "Name of product",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+              children: [
+                Text("25", style: TextStyle(color: Colors.red)),
+                Text("26", style: TextStyle(color: Colors.amber)),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,37 +1,13 @@
 import 'package:flutter/material.dart';
 
+import 'dashboard.dart';
+import 'login_page.dart';
+
 void main() {
-  runApp(
-    const MyApp(),
-    //     width: 100,
-    //     height: 50,
-    //     color: Colors.amber,
-    //     padding: EdgeInsets.all(20),
-    //     //give paricular padding in
-    //     //padding: EdgeInsets.only(left: 10, top: 5), //inside
-    //     margin: EdgeInsets.all(20), //outside
-    //     //red,black,green
-    //     //for single container or child
-    //     child: Container(width: 10, height: 10, color: Colors.blue),
-
-    //     //for column wise
-    //     // child: Column(
-    //     //   children: [
-    //     //     Container(width: 100, height: 100, color: Colors.blue),
-    //     //     Container(width: 10, height: 10, color: Colors.black),
-    //     //     Container(width: 10, height: 10, color: Colors.green),
-    //     //   ],
-    //     // ),
-    //   ),
-    //   // Row(
-    //   //   children: [
-    //   //     Container(color: Colors.black),
-    //   //     Container(color: Colors.red),
-    //   //   ],
-    //   // ),
-  );
+  runApp(const MyApp());
 }
-
+// design product card which contains image of product name of
+// it actual price and current and current price
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -39,30 +15,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Ecom App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const LoginPage(),
-    );
-  }
-}
-
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text("Login", style: TextStyle(fontSize: 50, color: Colors.purple)),
-          Text("user page"),
-          Text("Password"),
-          TextField(),
-          ElevatedButton(onPressed: () {}, child: Text("Login")),
-        ],
-      ),
+      home: Dashboard(),
     );
   }
 }
