eed8c79c414791fc2e72cf7829ca65767fa34ec7
Aditya Dodiya <aadityadodiya01@gmail.com>
2025-07-17 17:02:47 +0530

    add files and changes in code

--- flutter_application_1/lib/create_card.dart
+++ flutter_application_1/lib/create_card.dart
@@ -1,3 +1,5 @@
+// import 'dart:ui_web';
+
 import 'package:flutter/material.dart';
 
 import 'cart_page.dart';
@@ -8,19 +10,82 @@ class instacard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: 500,
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Container(height: 50, width: 50, color: Colors.lightBlue),
-                Text("  Chennaiipl", style: TextStyle(fontSize: 20)),
-              ],
-            ),
-            Container(height: 300, color: Colors.black),
-            Row(children: [Icon(Icons.library_add)]),
-          ],
+      body: Padding(
+        padding: const EdgeInsets.only(top: 20, bottom: 20),
+        child: Container(
+          color: Colors.black87,
+          height: 800,
+          child: Column(
+            children: [
+              SizedBox(height: 10),
+              Row(
+                children: [
+                  Container(
+                    height: 80,
+                    width: 80,
+                    decoration: BoxDecoration(
+                      color: Colors.blueGrey,
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Container(
+                    height: 80,
+                    width: 80,
+                    decoration: BoxDecoration(
+                      color: Colors.blueGrey,
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Container(
+                    height: 80,
+                    width: 80,
+                    decoration: BoxDecoration(
+                      color: Colors.blueGrey,
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Container(
+                    height: 80,
+                    width: 80,
+                    decoration: BoxDecoration(
+                      color: Colors.blueGrey,
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              Row(
+                children: [
+                  Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text(
+                    " icc",
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 5,
+                  top: 15,
+                  bottom: 15,
+                  right: 5,
+                ),
+                child: Container(height: 350, color: Colors.black),
+              ),
+              Row(),
+            ],
+          ),
         ),
       ),
     );
