f2a4136b2508a316fb7902636f59d162564d3b5e
Aditya Dodiya <aadityadodiya01@gmail.com>
2025-07-24 09:40:43 +0530

    add files and code

---
+++ flutter_application_1/lib/nodes_app/screens/notes_screen.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/model/note.dart';
+
+class NotesScreen extends StatefulWidget {
+  const NotesScreen({super.key});
+
+  @override
+  State<NotesScreen> createState() => _NotesScreenState();
+}
+
+class _NotesScreenState extends State<NotesScreen> {
+  TextEditingController titleTxtCntrl = TextEditingController();
+  TextEditingController descTxtCntrl = TextEditingController();
+  List<Note> notes = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Notes App')),
+      body: Column(
+        children: [
+          // add
+          TextField(
+            controller: titleTxtCntrl,
+            decoration: InputDecoration(
+              hintText: 'Enter title',
+              labelText: 'Title',
+            ),
+          ),
+          TextField(
+            controller: descTxtCntrl,
+            decoration: InputDecoration(
+              hintText: 'Enter Description',
+              labelText: 'Description',
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              // add logic
+              if (titleTxtCntrl.text.isNotEmpty &&
+                  descTxtCntrl.text.isNotEmpty) {
+                notes.add(
+                  Note(
+                    title: titleTxtCntrl.text,
+                    description: descTxtCntrl.text,
+                  ),
+                );
+                titleTxtCntrl.clear();
+                descTxtCntrl.clear();
+                setState(() {});
+              } else {}
+            },
+            child: Text('Add'),
+          ),
+          // display
+          Expanded(
+            child: ListView.builder(
+              itemCount: notes.length,
+              itemBuilder: (context, index) => ListTile(
+                title: Text('${notes[index].title}'),
+                subtitle: Text('${notes[index].description}'),
+                trailing: Icon(Icons.delete),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
