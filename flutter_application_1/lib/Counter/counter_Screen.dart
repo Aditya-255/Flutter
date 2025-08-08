dff3a56e40faa258e875390210ec9b9f32af8d4c
Aditya Dodiya <aadityadodiya01@gmail.com>
2025-08-08 08:58:53 +0530

    counter screen

--- flutter_application_1/lib/Counter/counter_Screen.dart
+++ flutter_application_1/lib/Counter/counter_Screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/Counter/counter_controller.dart';
 import 'package:get/get.dart';
-// import 'package:get/state_manager.dart';
 
+// import 'package:get/state_manager.dart';
+//create Quiz app with following screen 1.start with instruction and strart button
+// 2.quiz screen diplay que and 4 options else button also on top display score and bottom next complete
+//3.to display quiz score with details 
 class Counter extends StatelessWidget {
   CounterController controller = Get.find<CounterController>();
   @override
@@ -16,7 +19,7 @@ class Counter extends StatelessWidget {
           Obx(
             () => Text(
               controller.count.value.toString(),
-              style: TextStyle(fontSize: 40),
+              style: TextStyle(fontSize: 30),
             ),
           ),
           Row(
@@ -30,7 +33,7 @@ class Counter extends StatelessWidget {
                 onPressed: () {
                   controller.increment();
                 },
-                child: Icon(Icons.add, size: 50),
+                child: Icon(Icons.add, size: 30),
               ),
               ElevatedButton(
                 style: ElevatedButton.styleFrom(
@@ -40,7 +43,7 @@ class Counter extends StatelessWidget {
                 onPressed: () {
                   controller.decrement();
                 },
-                child: Icon(Icons.remove, size: 50),
+                child: Icon(Icons.remove, size: 30),
               ),
               ElevatedButton(
                 style: ElevatedButton.styleFrom(
@@ -50,7 +53,7 @@ class Counter extends StatelessWidget {
                 onPressed: () {
                   controller.reset();
                 },
-                child: Icon(Icons.undo, size: 50),
+                child: Icon(Icons.undo, size: 30),
               ),
             ],
           ),
